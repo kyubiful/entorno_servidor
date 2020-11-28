f01deedf86853849d41bd914784357a0736035cd
Kyubiful <serzmr@gmail.com>
2020-11-28 13:31:56 +0100

    Inicio de proyecto + Tema 2 y Tema 3

---
+++ tema_3/tarea_1/tarea1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <title>Tarea 1</title>
+  </head>
+  <body>
+    <?php
+      //Defino las variables
+      $name='Thomas Middleditch';
+      $age=38;
+      $isAlive=false;
+    ?>
+      <p><?php echo 'El nombre de mi personaje es: '.$name; ?></p>
+      <p><?php echo 'La edad de mi personaje es: '.$age; ?></p>
+      <p><?php 
+	if($isAlive){
+	  echo 'Mi personaje está vivo';
+	} else {
+	  echo 'Mi personaje NO está vivo';
+	}
+       ?></p>
+  </body>
+</html>
